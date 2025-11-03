0013ebd8694cf0bb4ea222568e59d35bf4302ed0
Julien Duseyau <julien.duseyau@gmail.com>
2025-11-03 10:28:53 +0100

    Override default RequestHandlerInterface and dispatcher middleware in RouteServiceProvider

--- src/Provider/RouteServiceProvider.php
+++ src/Provider/RouteServiceProvider.php
@@ -7,13 +7,16 @@ namespace Maduser\Argon\Routing\Provider;
 use Maduser\Argon\Container\AbstractServiceProvider;
 use Maduser\Argon\Container\ArgonContainer;
 use Maduser\Argon\Container\Exceptions\ContainerException;
+use Maduser\Argon\Middleware\Contracts\Middleware\DispatcherInterface;
 use Maduser\Argon\Middleware\Contracts\PipelineManagerInterface;
 use Maduser\Argon\Middleware\MiddlewarePipeline;
+use Maduser\Argon\Prophecy\Support\Tag;
 use Maduser\Argon\Routing\Contracts\RequestHandlerResolverInterface;
 use Maduser\Argon\Routing\Contracts\RouteMatcherInterface;
 use Maduser\Argon\Routing\Contracts\RouterInterface;
 use Maduser\Argon\Routing\Contracts\RouteStoreInterface;
 use Maduser\Argon\Routing\Factory\RoutingRequestHandlerFactory;
+use Maduser\Argon\Routing\Middleware\RouteDispatcherMiddleware;
 use Maduser\Argon\Routing\Middleware\RouteMatcherMiddleware;
 use Maduser\Argon\Routing\RequestHandlerResolver;
 use Maduser\Argon\Routing\RouteManager;
@@ -42,11 +45,20 @@ class RouteServiceProvider extends AbstractServiceProvider
 
         $container->set(RouteMatcherInterface::class, RouteMatcher::class);
 
+        /**
+         * Override the default RequestHandlerInterface
+         */
         $container->set(RequestHandlerResolverInterface::class, RequestHandlerResolver::class);
 
         $container->set(RoutingRequestHandlerFactory::class);
 
         $container->set(RequestHandlerInterface::class, MiddlewarePipeline::class)
             ->factory(RoutingRequestHandlerFactory::class, 'create');
+
+        /**
+         * Override the default dispatcher middleware
+         */
+        $container->set(DispatcherInterface::class, RouteDispatcherMiddleware::class)
+            ->tag([Tag::MIDDLEWARE_HTTP => ['priority' => 6000, 'group' => ['api', 'web']]]);
     }
 }
